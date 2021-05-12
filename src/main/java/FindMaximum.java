562aad8229460dca6bc4ff55f08ae329982faa5a
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-05-12 22:19:35 +0530

    Added print function using generics to display message

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -25,8 +25,16 @@ public class FindMaximum<Z extends Comparable> {
         if (z.compareTo(max) > 0) {
             max = z;
         }
-        System.out.println("<--- The Maximum Element is: " + max + " --->");
+        print(x, y, z, max);
         return max;
+    }
+
+    public static <Z> void print(Z x, Z y, Z z, Z max) {
+        System.out.println("<---- Maximum element from Values x = " + x + " y = " + y + " z = " + z + " is = " + max + "---->");
+    }
+
+    public static void main(String[] args) {
+        FindMaximum.printMax(1, 2, 3);
     }
 
     public Z testMaximum(Z x, Z y, Z z) {
