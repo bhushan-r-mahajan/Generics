cd0254cf259938605055d08a2c516ebd6bc09dd8
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-05-12 17:42:02 +0530

    Refactored the code to use a generic method

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,31 +1,7 @@
 public class FindMaximum {
 
-    public static Integer printMaxInteger(Integer x, Integer y, Integer z) {
-        Integer max = x;
-        if(y.compareTo(max) > 0) {
-            max = y;
-        }
-        if(z.compareTo(max) > 0) {
-            max = z;
-        }
-        System.out.println("<---The Maximum Element is: " + max + " --->");
-        return max;
-    }
-
-    public static Double printMaxDouble(Double x, Double y, Double z) {
-        Double max = x;
-        if(y.compareTo(max) > 0) {
-            max = y;
-        }
-        if(z.compareTo(max) > 0) {
-            max = z;
-        }
-        System.out.println("<---The Maximum Element is: " + max + " --->");
-        return max;
-    }
-
-    public static String printMaxString(String x, String y, String z) {
-        String max = x;
+    public static <Z extends Comparable<Z>> Z printMax(Z x, Z y, Z z) {
+        Z max = x;
         if(y.compareTo(max) > 0) {
             max = y;
         }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -5,63 +5,63 @@ public class FindMaximumTest {
     @Test
     void givenIntegerValueTestMaxInFirstPlace() {
         FindMaximum m = new FindMaximum();
-        Integer result = m.printMaxInteger(20,11,7);
+        Integer result = m.printMax(20,11,7);
         Assertions.assertEquals( 20, result);
     }
 
     @Test
     void givenIntegerValueTestMaxInSecondPlace() {
         FindMaximum m = new FindMaximum();
-        Integer result = m.printMaxInteger(2,11,7);
+        Integer result = m.printMax(2,11,7);
         Assertions.assertEquals( 11, result);
     }
 
     @Test
     void givenIntegerValueTestMaxInThirdPlace() {
         FindMaximum m = new FindMaximum();
-        Integer result = m.printMaxInteger(2,11,17);
+        Integer result = m.printMax(2,11,17);
         Assertions.assertEquals( 17, result);
     }
 
     @Test
     void givenDoubleValueTestMaxInFirstPlace() {
         FindMaximum m = new FindMaximum();
-        Double result = m.printMaxDouble(20.25,11.95,7.32);
+        Double result = m.printMax(20.25,11.95,7.32);
         Assertions.assertEquals( 20.25, result);
     }
 
     @Test
     void givenDoubleValueTestMaxInSecondPlace() {
         FindMaximum m = new FindMaximum();
-        Double result = m.printMaxDouble(2.3,11.77,7.33);
+        Double result = m.printMax(2.3,11.77,7.33);
         Assertions.assertEquals( 11.77, result);
     }
 
     @Test
     void givenDoubleValueTestMaxInThirdPlace() {
         FindMaximum m = new FindMaximum();
-        Double result = m.printMaxDouble(2.55,11.33,17.99);
+        Double result = m.printMax(2.55,11.33,17.99);
         Assertions.assertEquals( 17.99, result);
     }
 
     @Test
     void givenStringValueTestMaxInFirstPlace() {
         FindMaximum m = new FindMaximum();
-        String result = m.printMaxString("watermelon","banana","apple");
+        String result = m.printMax("watermelon","banana","apple");
         Assertions.assertEquals( "watermelon", result);
     }
 
     @Test
     void givenStringValueTestMaxInSecondPlace() {
         FindMaximum m = new FindMaximum();
-        String result = m.printMaxString("apple","watermelon","banana");
+        String result = m.printMax("apple","watermelon","banana");
         Assertions.assertEquals( "watermelon", result);
     }
 
     @Test
     void givenStringValueTestMaxInThirdPlace() {
         FindMaximum m = new FindMaximum();
-        String result = m.printMaxString("apple","banana","watermelon");
+        String result = m.printMax("apple","banana","watermelon");
         Assertions.assertEquals( "watermelon", result);
     }
 }
